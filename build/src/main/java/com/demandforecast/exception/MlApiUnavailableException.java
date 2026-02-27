c304b77e8865af178a2330db5367b55024e065de
maddyy3101 <maddyy931@gmail.com>
2026-02-27 12:42:25 +0530

    chore: remove local/build files from repository

--- build/src/main/java/com/demandforecast/exception/MlApiUnavailableException.java
+++
@@ -1,9 +0,0 @@
-package com.demandforecast.exception;
-
-public class MlApiUnavailableException extends DemandForecastException {
-    public MlApiUnavailableException(Throwable cause) {
-        super("ML_API_UNAVAILABLE",
-              "The ML inference service is currently unavailable. Please try again later.",
-              cause);
-    }
-}
